aea1fc1e49a8e0f187f9569e828510759cc3f539
Domenico <sviluppo@editt.it>
2017-05-31 12:43:38 +0200

    Added "signature" for getController method

--- Module.php
+++ Module.php
@@ -89,4 +89,9 @@ class Module{
 	 * A loaded module can, optionally, put content in the "head" section of the page
 	 */
 	public function headings(){}
+
+	/**
+	 * Meant to be extended if needed
+	 */
+	public function getController($request, $rule){}
 }
