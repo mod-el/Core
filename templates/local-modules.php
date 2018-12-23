f41066a0bf8ce00071e3d1e4fb4293e9618063fb
Domenico Giambra <zorinik@gmail.com>
2018-12-23 17:14:49 +0100

    New css classes in "local modules" page as well

--- templates/local-modules.php
+++ templates/local-modules.php
@@ -2,14 +2,12 @@
 
 <div style="font-size: 0">
 	<?php foreach ($modules as $m) { ?>
-		<div class="modulo">
-			<div>
+		<div class="module-cont">
+			<div class="module">
 				<div class="clickable" onclick="document.location.href='<?= PATH ?>zk/local-modules/<?= entities($m->folder_name) ?>'">
-					<div>
-						<div class="versione"><?= entities($m->version) ?></div>
-						<b><?= entities($m->name) ?></b>
-						<?php if ($m->description) { ?><p><i><?= entities($m->description, true) ?></i></p><?php } ?>
-					</div>
+					<div class="module-version"><?= entities($m->version) ?></div>
+					<b><?= entities($m->name) ?></b>
+					<?php if ($m->description) { ?><p><i><?= entities($m->description, true) ?></i></p><?php } ?>
 				</div>
 			</div>
 		</div>
