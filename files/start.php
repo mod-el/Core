185d1e60f4aab25090a6e8cacba2f7df60e1a33d
Domenico Giambra <zorinik@gmail.com>
2019-06-16 15:00:29 +0200

    New (customizable) costants "HTTPS" and "BASE_HOST"

--- files/start.php
+++ files/start.php
@@ -1,19 +1,19 @@
 <?php
 session_start();
 
-$baseDirectory = realpath(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..').DIRECTORY_SEPARATOR;
+$baseDirectory = realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR;
 
 // Setup the Autoloader
-include_once($baseDirectory.'model'.DIRECTORY_SEPARATOR.'Core'.DIRECTORY_SEPARATOR.'Autoloader.php');
+include_once($baseDirectory . 'model' . DIRECTORY_SEPARATOR . 'Core' . DIRECTORY_SEPARATOR . 'Autoloader.php');
 
-\Model\Core\Autoloader::setNamespace('Model\\', $baseDirectory.'model');
-\Model\Core\Autoloader::setNamespace('Model\\', $baseDirectory.'app');
+\Model\Core\Autoloader::setNamespace('Model\\', $baseDirectory . 'model');
+\Model\Core\Autoloader::setNamespace('Model\\', $baseDirectory . 'app');
 
-spl_autoload_register(function($className){
+spl_autoload_register(function ($className) {
 	return \Model\Core\Autoloader::autoload($className);
 });
 
 // Utility functions
 include_once('func.php');
 // Custom functions
-include_once($baseDirectory.'app/func-extra.php');
+include_once($baseDirectory . 'app/func-extra.php');
